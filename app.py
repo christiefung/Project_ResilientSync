66a66f97b9f534490349bb190708917243e5ab39
christiefung <fkmchristie@gmail.com>
2026-04-10 15:52:08 -0700

    data product refine

--- app.py
+++ app.py
@@ -45,16 +45,39 @@ if st.button("Predict Burnout Risk", type="primary"):
     risk = model.predict(input_df)[0]
     
     st.divider()
+
+    st.subheader("Strategic Management Plan")
     
-    # Results
+    # Instead of showing results in employees' perspectives, provide recommendations and actions based on the risk level:
     if risk == 0:
         st.success("### ✅ Low Burnout Risk")
-        st.write("You are showing strong resilience markers.")
+        st.write("**Action:** This employee is a 'Reilience Syncer' - they are adapting well to AI integration." \
+        "Encourage them to maintain their current habits and consider pairing them with more anxious junior staff and sharing their strategies with them.")
     elif risk == 1:
         st.warning("### ⚠️ Moderate Burnout Risk")
-        st.write("Consider monitoring your cognitive load and prioritizing sleep.")
+        if anxiety >= 7:
+            st.write("**Action:** Authorize a Low-Risk Strategic Pilot. Task the employee with solving a real workflow bottleneck using AI.")
+            
+            with st.expander("The Strategic Intervention"):
+                st.markdown("""
+                            **Objective:** Restore professional security by proving human indispensability.
+                            * **Hybrid Project Assignment:** Task the employee with project where AI handles the 'grunt work' (e.g., drafting/data cleanup) whereas the employee provides the strategic pivot.'
+                            * **The 'AI Supervisor' Role:** By letting them *optimize* the tool for their own job, they prove to themselves that the tool is a utility they control, not a replacement that controls them.
+                            * **Output:** The end result is a new guide that the employee can present to the team, boosting their status as an AI-subject matter expert and restoring their sense of professional value.
+                            """)
+        
+        else:
+            st.write("**Action:** Focus on biological resilience - encourage better sleep, regular breaks, and physical activity to help manage cognitive load. " \
+            "Evaluate if evening AI-usage is bleeding into sleep time and encourage setting boundaries around work hours.")
     else:
         st.error("### 🚨 High Burnout Risk")
-        st.write("High risk detected. It may be time to address AI anxiety or workflow overload.")  
+        if experience < 5:
+            st.write("**Action:** Identity Anchoring: This employee feels their core value is threatened. Managers should explicitly decouple their " \
+            "professional worth from task-automation and focus on their 'human-in-the-loop' creative oversight. ")
+        else:
+            st.write("**Action:** Cognitive Offloading: This senior professional is likely over-extending. Audit their 'Interruptions Per Day' and enforce deep-work blocks.")  
 
+
+
+ 
 
